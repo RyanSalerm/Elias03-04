ba1e78712a02934f25f70feb07c6da224ddb8f9f
RyanSalerm <123087789+RyanSalerm@users.noreply.github.com>
2024-04-02 07:34:28 -0300

    Add files via upload

---
+++ merge03-04_estrutura.h
@@ -0,0 +1,7 @@
+typedef struct {
+    char uf[10];
+    int casosGraves23;
+    int obitos23;
+    int casosGraves24;
+    int obitos24;
+}No;
---
+++ merge03-04_funcao.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void arquivoTXT()
+{
+    fopen();
+}
+void mergeSort(int vetor[], int quantidade)
+{
+
+}
